3a94daae0366340ab109d2e62073127294077628
Riccardo Cogoni <67969874+rico157@users.noreply.github.com>
2025-03-14 15:29:11 +0000

    static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
+import staticAdapter from "@astrojs/vercel/static";
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel()
+  output: "static",
+  adapter: staticAdapter(),
 });
